d34768748466d6ed91c5ea0da677265ed342e841
mohammadkhoddami <mohammadh.khoddami@gmail.com>
2024-07-18 04:07:32 +0330

    create class for our admin.py

--- tracking/admin.py
+++ tracking/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import ApiRequestLog
 # Register your models here.
+@admin.register(ApiRequestLog)
+class ApiRequestLogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'datetime_requested_at', 'response_ms', 'status_code',
+                    'user', 'method',
+                    'path', 'remote_address', 'host',
+                    'query_params'
+                    )
