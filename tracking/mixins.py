10a199458ced76d7ea9a5c0372a486ca6963601e
mohammadkhoddami <mohammadh.khoddami@gmail.com>
2024-07-24 04:36:04 +0330

    just correct words !

--- tracking/mixins.py
+++ tracking/mixins.py
@@ -1,6 +1,6 @@
 from .base_mixins import BaseLoggingMixin
 from .models import ApiRequestLog
 class LoggingMixin(BaseLoggingMixin):
-    def heandle_log(self):
+    def handle_log(self):
         # ApiRequestLog(**self.log).save()
         print(self.log)
