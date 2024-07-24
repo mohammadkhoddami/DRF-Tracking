c5c9fbcca765b33ae7e8a353675817ea34a76d88
mohammadkhoddami <mohammadh.khoddami@gmail.com>
2024-07-24 04:23:45 +0330

    clean it!

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #installed packages
+    'rest_framework',
+    
+    #local apps
     'tracking',
 ]
 
