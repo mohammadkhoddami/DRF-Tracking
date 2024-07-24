f3ef9b69295c252ff0a9d4f7b0dc5e90366cf687
mohammadkhoddami <mohammadh.khoddami@gmail.com>
2024-07-24 04:33:03 +0330

    add urls and views for testing the module

--- tracking/views.py
+++ tracking/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .mixins import LoggingMixin
+class HomeView(LoggingMixin, APIView):
+    def get(self, request):
+        return Response('hello', status=status.HTTP_200_OK)
